d6d1c108055e45bfe7840f5e56333ce8bc5b6cf1
ajkuo <anjiekuo@gmail.com>
2017-11-13 22:04:26 +0800

    Site updated: 2017-11-13 22:04:26

--- js/main.js
+++ js/main.js
@@ -1,106 +1,117 @@
-/**
- * Sets up Justified Gallery.
- */
-if (!!$.prototype.justifiedGallery) {
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: 'justify'
-  };
-  $('.article-gallery').justifiedGallery(options);
-}
+(function () {
+  var searchEl = document.querySelector('.search-do'),
+      searchWrap = document.querySelector('.search-wrap'),
+      searchContent = document.querySelector('.search-content'),
+      inputEl = searchWrap.querySelector('input'),
+      resultEl = searchWrap.querySelector('.search-result'),
+      oldResultElString = resultEl.innerHTML,
+      postData;
+  
+  function showSearch (e) {
+    e.preventDefault();
+    searchWrap.style.display = 'flex';
+    inputEl.focus();
+    getSearchData();
+  }
 
+  function hideSearch () {
+    searchWrap.style.display = 'none';
+    resultEl.innerHTML = oldResultElString;
+    postData = null;
+  }
 
-$(document).ready(function() {
+  function indexOfIgnoreTransform (a, b) {
+    a = (a || '').toLocaleLowerCase();
+    b = (b || '').toLocaleLowerCase();
 
-  /**
-   * Shows the responsive navigation menu on mobile.
-   */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
+    return a.indexOf(b) > -1;
+  }
 
-
-  /**
-   * Controls the different versions of  the menu in blog post articles 
-   * for Desktop, tablet and mobile.
-   */
-  if ($(".post").length) {
-    /**
-     * Display the menu if the menu icon is clicked.
-     */
-    var menu = $("#menu");
-    var menu_icon = $("#menu-icon, #menu-icon-tablet");
-    menu_icon.click(function() {
-      if (menu.css('visibility') === 'hidden') {
-        menu.css("visibility", "visible");
-        menu_icon.addClass('active');
-      } else {
-        menu.css("visibility", "hidden");
-        menu_icon.removeClass('active');
+  function searchFromJson(data, key) {
+    if (!data || data.length < 1 || !key) {
+      return;
+    }
+    return data.filter(function(post) {
+      if (indexOfIgnoreTransform(post.title, key)) {
+        return true;
       }
-      return false;
+      if (indexOfIgnoreTransform(post.content, key)) {
+        return true;
+      }
+    })
+  }
+
+  function render(data) {
+    if (!data || data.length < 1) {
+      return resultEl.innerHTML = '<div class="search-no">NOT FIND!</div>';
+    }
+    var htmlString = '',
+      tempEl = document.createElement('div');
+
+    data.forEach(function(post) {
+      tempEl.innerHTML = (post.content || '').slice(0, 100)
+      htmlString += '<li data-url="' + post.url + '"' + '><h3>' + 
+        post.title + '</h3><p>' + tempEl.innerText + '</p></li>'
     });
 
-    /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
-     */
-    if (menu.length) {
-      $(window).on('scroll', function() {
-        var topDistance = $("#menu > #nav").offset().top;
+    resultEl.innerHTML = htmlString;
+  }
 
-        // hide only the navigation links on desktop
-        if (menu.css('visibility') !== 'hidden' && topDistance < 50) {
-          $("#menu > #nav").show();
-        } else if (menu.css('visibility') !== 'hidden' && topDistance > 100) {
-          $("#menu > #nav").hide();
-        }
-
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
-        }
-      });
+  function getSearchData (callback) {
+    if (postData) {
+      callback && callback(postData);
+      return;
     }
+    fetch('/search.json').then(function (respons) {
+      respons.json().then(function(data){
+        postData = data;
+        callback && callback(postData);
+      }); 
+    }).catch(function() {
+      callback && (resultEl.innerHTML = 'An error has occurred！');
+    });
+  }
 
-    /**
-     * Show mobile navigation menu after scrolling upwards,
-     * hide it again after scrolling downwards.
-     */
-    if ($( "#footer-post").length) {
-      var lastScrollTop = 0;
-      $(window).on('scroll', function() {
-        var topDistance = $(window).scrollTop();
+  function doSearch (e) {
+    if (e.isComposing) {
+      return;
+    }
+    if (!e.target.value) {
+      resultEl.innerHTML = oldResultElString;
+      return;
+    }
+    getSearchData(function(data){
+      render(searchFromJson(data, e.target.value));
+    });
+  }
 
-        if (topDistance > lastScrollTop){
-          // downscroll -> show menu
-          $("#footer-post").hide();
-        } else {
-          // upscroll -> hide menu
-          $("#footer-post").show();
-        }
-        lastScrollTop = topDistance;
+  function goto (url) {
+    window.location.href = url;
+  }
 
-        // close all submenu's on scroll
-        $("#nav-footer").hide();
-        $("#toc-footer").hide();
-        $("#share-footer").hide();
+  function gotoSearchResult(e) {
+    e.stopPropagation();
+    var el = e.target;
 
-        // show a "navigation" icon when close to the top of the page, 
-        // otherwise show a "scroll to the top" icon
-        if (topDistance < 50) {
-          $("#actions-footer > ul > #top").hide();
-          $("#actions-footer > ul > #menu").show();
-        } else if (topDistance > 100) {
-          $("#actions-footer > ul > #menu").hide();
-          $("#actions-footer > ul > #top").show();
-        }
-      });
+    if (el.tagName !== 'LI') {
+      el = el.parentNode;
+    }
+    if (el.tagName !== 'LI' || !el.dataset.url) {
+      return;
+    }
+    goto(window.location.origin + el.dataset.url);
+  }
+
+  function onDocKeydown (e) {
+    if (e.keyCode === 27) {
+      hideSearch();
     }
   }
-});
+
+  searchEl.addEventListener('click', showSearch);
+  inputEl.addEventListener('input', doSearch);
+  searchWrap.addEventListener('click', hideSearch);
+  searchContent.addEventListener('click', gotoSearchResult);
+  inputEl.addEventListener('compositionend', doSearch);
+  document.addEventListener('keydown', onDocKeydown);
+})();
